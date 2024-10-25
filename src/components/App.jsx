155df3af2860e25e94b9bf6970c424d895acc098
Yevhenii Baranik <baranik18@gmail.com>
2024-10-25 22:17:49 +0100

    init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import Profile from "./Profile/Profile";
 import userData from "../userData.json";
-import FriendList from "./FriendList/FriendList";
+import FriendList from "./FriendListItem/FriendListItem";
 import "../index.css";
 import friends from "../friendData.json";
 import "modern-normalize";
