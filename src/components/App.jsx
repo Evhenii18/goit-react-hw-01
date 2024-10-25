5856b89da36ef3eda342c3898f5badb258bbd9a9
Yevhenii Baranik <baranik18@gmail.com>
2024-10-25 21:26:16 +0100

    init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 import Profile from "./Profile/Profile";
 import userData from "../userData.json";
-import FriendList from "./FriendList/FriendList";
+import FriendList from "./FriendList/FriendListt";
 import "../index.css";
-import friends from "../friendsData.json";
+import friends from "../friendData.json";
 import "modern-normalize";
 import TransactionHistory from "./TransactionHistory/TransactionHistory";
-import transactions from "../transactions.json";
+import transactions from "../transaction.json";
 
 const App = () => {
 	return (
